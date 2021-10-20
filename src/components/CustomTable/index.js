fd91943e9f6deaa33ce0bd9ba65e89df2f650faf
sakibSpaceCode <sakib.khan@spacecode.com>
2021-10-20 22:26:53 +0530

    changes

--- src/components/CustomTable/index.js
+++ src/components/CustomTable/index.js
@@ -33,29 +33,36 @@ const columns = [
     align: "right",
     format: (value) => value.toFixed(2),
   },
+  {
+    id: "status",
+    label: "Status",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
 ];
 
-function createData(name, code, population, size) {
+function createData(name, code, population, size, status) {
   const density = population / size;
-  return { name, code, population, size, density };
+  return { name, code, population, size, density, status };
 }
 
 const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
+  createData("India", "IN", 1324171354, 3287263, "Completed"),
+  createData("China", "CN", 1403500365, 9596961, "Completed"),
+  createData("Italy", "IT", 60483973, 301340, "Pending"),
+  createData("United States", "US", 327167434, 9833520, "Completed"),
+  createData("Canada", "CA", 37602103, 9984670, "Completed"),
+  createData("Australia", "AU", 25475400, 7692024, "Completed"),
+  createData("Germany", "DE", 83019200, 357578, "Pending"),
+  createData("Ireland", "IE", 4857000, 70273, "Completed"),
+  createData("Mexico", "MX", 126577691, 1972550, "Pending"),
+  createData("Japan", "JP", 126317000, 377973, "Completed"),
+  createData("France", "FR", 67022000, 640679, "Completed"),
+  createData("United Kingdom", "GB", 67545757, 242495, "Pending"),
+  createData("Russia", "RU", 146793744, 17098246, "Completed"),
+  createData("Nigeria", "NG", 200962417, 923768, "Pending"),
+  createData("Brazil", "BR", 210147125, 8515767, "Pending"),
 ];
 
 const useStyles = makeStyles({
@@ -112,11 +119,17 @@ export default function CustomTable() {
                 <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
                   {columns.map((column) => {
                     const value = row[column.id];
-                    return (
+                    return value === "Completed" ? (
                       <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === "number"
-                          ? column.format(value)
-                          : value}
+                        {value}
+                      </TableCell>
+                    ) : value === "Pending" ? (
+                      <TableCell key={column.id} align={column.align}>
+                        {value}
+                      </TableCell>
+                    ) : (
+                      <TableCell key={column.id} align={column.align}>
+                        {value}
                       </TableCell>
                     );
                   })}
