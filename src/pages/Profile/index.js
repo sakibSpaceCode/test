0df49cf5db97da777da9aff05a0d128cc2e37888
Ankit Pavar <ankit.pavar@spacecode.com>
2022-01-09 22:48:03 +0530

    dd

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,139 +1,92 @@
+import { Card, Grid, InputLabel } from "@material-ui/core";
 import React from "react";
-import "./style.css";
-const ImgUpload = ({ onChange, src }) => (
-  <label htmlFor="photo-upload" className="custom-file-upload fas">
-    <div className="img-wrap img-upload">
-      <img for="photo-upload" src={src} />
-    </div>
-    <input id="photo-upload" type="file" onChange={onChange} />
-  </label>
-);
+import ImageUploader from "react-images-upload";
+import CustomButton from "../../components/button";
+import CustomInput from "../../components/CustomInput";
 
-const Name = ({ onChange, value }) => (
-  <div className="field">
-    <label htmlFor="name">name:</label>
-    <input
-      id="name"
-      type="text"
-      onChange={onChange}
-      maxlength="25"
-      value={value}
-      placeholder="Alexa"
-      required
-    />
-  </div>
-);
+const ProfilePage = () => {
+  const [image, setImage] = React.useState(null);
 
-const Status = ({ onChange, value }) => (
-  <div className="field">
-    <label htmlFor="status">status:</label>
-    <input
-      id="status"
-      type="text"
-      onChange={onChange}
-      maxLength="35"
-      value={value}
-      placeholder="It's a nice day!"
-      required
-    />
-  </div>
-);
-
-const Profile = ({ onSubmit, src, name, status }) => (
-  <div className="card">
-    <form onSubmit={onSubmit}>
-      <h1>Profile Card</h1>
-      <label className="custom-file-upload fas">
-        <div className="img-wrap">
-          <img for="photo-upload" src={src} />
-        </div>
-      </label>
-      <div className="name">{name}</div>
-      <div className="status">{status}</div>
-      <button type="submit" className="edit">
-        Edit Profile{" "}
-      </button>
-    </form>
-  </div>
-);
-
-const Edit = ({ onSubmit, children }) => (
-  <div className="card">
-    <form onSubmit={onSubmit}>
-      <h1>Profile Card</h1>
-      {children}
-      <button type="submit" className="save">
-        Save{" "}
-      </button>
-    </form>
-  </div>
-);
-
-class ProfilePage extends React.Component {
-  state = {
-    file: "",
-    imagePreviewUrl:
-      "https://github.com/OlgaKoplik/CodePen/blob/master/profile.jpg?raw=true",
-    name: "",
-    status: "",
-    active: "edit",
+  const onDrop = (e) => {
+    console.log(e?.[0]);
+    setImage(e?.[0]);
+    // setState({
+    //   pictures: state.pictures.concat(
+    //     picture
+    //   ) /* state.pictures.concat(picture) */
+    // });
   };
-
-  photoUpload = (e) => {
-    e.preventDefault();
-    const reader = new FileReader();
-    const file = e.target.files[0];
-    reader.onloadend = () => {
-      this.setState({
-        file: file,
-        imagePreviewUrl: reader.result,
-      });
-    };
-    reader.readAsDataURL(file);
-  };
-  editName = (e) => {
-    const name = e.target.value;
-    this.setState({
-      name,
-    });
-  };
-
-  editStatus = (e) => {
-    const status = e.target.value;
-    this.setState({
-      status,
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let activeP = this.state.active === "edit" ? "profile" : "edit";
-    this.setState({
-      active: activeP,
-    });
-  };
-
-  render() {
-    const { imagePreviewUrl, name, status, active } = this.state;
-    return (
-      <div>
-        {active === "edit" ? (
-          <Edit onSubmit={this.handleSubmit}>
-            <ImgUpload onChange={this.photoUpload} src={imagePreviewUrl} />
-            <Name onChange={this.editName} value={name} />
-            <Status onChange={this.editStatus} value={status} />
-          </Edit>
-        ) : (
-          <Profile
-            onSubmit={this.handleSubmit}
-            src={imagePreviewUrl}
-            name={name}
-            status={status}
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={4}>
+        <Card
+          style={{
+            padding: 20,
+            borderRadius: 10,
+            backgroundColor: "#d0d0d0",
+          }}>
+          <h1 style={{}}>Update Profile Picture</h1>
+          <ImageUploader
+            withIcon={true}
+            label={"Max file size: 5mb, accepted: jpg | png "}
+            buttonText='Choose image'
+            onChange={onDrop}
+            imgExtension={[".jpg", ".png"]}
+            maxFileSize={5242880}
+            singleImage={true}
+            accept='image/*'
+            withPreview={true}
           />
-        )}
-      </div>
-    );
-  }
-}
+          {image && (
+            <div style={{ marginLeft: 200 }}>
+              <CustomButton color='primary'>Submit</CustomButton>
+            </div>
+          )}
+        </Card>
+      </Grid>
+      <Grid item xs={6}>
+        <Card
+          style={{
+            padding: 20,
+            borderRadius: 10,
+            backgroundColor: "#d0d0d0",
+          }}>
+          <h1 style={{}}>Update Password</h1>
+          <Grid container direction='column' spacing={5}>
+            <Grid item md={6}>
+              <InputLabel>Old Password</InputLabel>
+              <CustomInput
+                onChange={(e) => console.log(e)}
+                name='oldPass'
+                value=''
+                autoFocus
+                fullWidth
+                style={{ width: 300 }}
+                // className={classes.textField}
+                // size={input.bigInput ? "lg" : "md"}
+              />
+            </Grid>
+            <Grid item md={6}>
+              <InputLabel>New Password</InputLabel>
+              <CustomInput
+                onChange={(e) => console.log(e)}
+                name='oldPass'
+                value=''
+                autoFocus
+                fullWidth
+                style={{ width: 300 }}
+                // className={classes.textField}
+                // size={input.bigInput ? "lg" : "md"}
+              />
+            </Grid>
+            <Grid item md={6}>
+              <CustomButton color='primary'>Submit</CustomButton>
+            </Grid>
+          </Grid>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default ProfilePage;
